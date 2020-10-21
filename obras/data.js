208c8a884885f2c87ffe713cf44f1a5069af3257
fabiohide <fheguchi@gmail.com>
2020-10-20 21:00:27 -0300

    Adicionada prévia do trabalho Paisagens Incidentais

---
+++ obras/data.js
@@ -0,0 +1,211 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-i_i",
+      "name": "I_I",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 3.1400318236593936,
+        "pitch": 0.009494143221195017,
+        "fov": 1.9864078399704563
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.12513960163828486,
+          "pitch": 0.2833280496697945,
+          "rotation": 0,
+          "target": "1-i_ii"
+        },
+        {
+          "yaw": 0.8457082478526701,
+          "pitch": 0.25789792566150993,
+          "rotation": 0,
+          "target": "2-i_iii"
+        },
+        {
+          "yaw": 1.5464541105183258,
+          "pitch": 0.32096471918992364,
+          "rotation": 0,
+          "target": "3-i_iv"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-i_ii",
+      "name": "I_II",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": -2.874859921977764,
+        "pitch": 0.020311533502237467,
+        "fov": 1.8892722212450992
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.582318115510155,
+          "pitch": 0.33801445918163786,
+          "rotation": 0,
+          "target": "2-i_iii"
+        },
+        {
+          "yaw": -0.9311819191059172,
+          "pitch": 0.2882476188064764,
+          "rotation": 0,
+          "target": "3-i_iv"
+        },
+        {
+          "yaw": -0.13333525934167412,
+          "pitch": 0.34086557451956523,
+          "rotation": 0,
+          "target": "0-i_i"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-i_iii",
+      "name": "I_III",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": -1.1983250194777995,
+        "pitch": -0.009638965330456273,
+        "fov": 1.8892681256000359
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.44333875847565274,
+          "pitch": 0.36599910957007253,
+          "rotation": 0,
+          "target": "3-i_iv"
+        },
+        {
+          "yaw": 1.926456583559439,
+          "pitch": 0.41024572679926585,
+          "rotation": 0,
+          "target": "2-i_iii"
+        },
+        {
+          "yaw": 0.5353488979559238,
+          "pitch": 0.3102913754092338,
+          "rotation": 0,
+          "target": "0-i_i"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-i_iv",
+      "name": "I_IV",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 2.6861766944419028,
+        "pitch": 0.027458463256092358,
+        "fov": 1.9864078399704563
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8001284254424323,
+          "pitch": 0.3689407059158505,
+          "rotation": 0,
+          "target": "2-i_iii"
+        },
+        {
+          "yaw": 0.8595512340026161,
+          "pitch": 0.2805926360597226,
+          "rotation": 0,
+          "target": "1-i_ii"
+        },
+        {
+          "yaw": -0.30953739507851985,
+          "pitch": 0.3859125971656159,
+          "rotation": 0,
+          "target": "0-i_i"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Fábio Hideki",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
