71f6ba1ea7443bc306af04519f9df196f839daaf
fabiohide <fheguchi@gmail.com>
2020-12-12 00:17:00 -0300

    Cordilheiras Andantes ajustes finais

--- trabalhos/sketch.js
+++ trabalhos/sketch.js
@@ -20,12 +20,9 @@ function setup() {
 
   
 
-  cenario_1 = new Cenario(imagemCena_1, imagemCena_2, 0.1,);
-  //cenario_2 = new Cenario(imagemCena_2, 1.5);
-  cenario_3 = new Cenario(imagemCena_3, imagemCena_4, 0.2,);
-  //cenario_4 = new Cenario(imagemCena_4, 2.5);
-  cenario_5 = new Cenario(imagemCena_5, imagemCena_5, 0.25,);
-  //cenario_6 = new Cenario(imagemCena_6, 3);
+  cenario_1 = new Cenario(imagemCena_1, imagemCena_2, 0.15,);
+  cenario_3 = new Cenario(imagemCena_3, imagemCena_4, 0.25,);
+  cenario_5 = new Cenario(imagemCena_5, imagemCena_5, 0.30,);
 
 
 }
@@ -35,23 +32,20 @@ function draw() {
 
   cenario_1.exibe();
   cenario_1.move();
-  //cenario_2.exibe();
-  //cenario_2.move();
   cenario_3.exibe();
   cenario_3.move();
-  //cenario_4.exibe();
-  //cenario_4.move();
   cenario_5.exibe();
   cenario_5.move();
 
 }
 
 function preload() {
+  imagemCena_5 = loadImage('modulos/layer5.png');
   imagemCena_1 = loadImage('modulos/layer1.png');
   imagemCena_2 = loadImage('modulos/layer2.png');
   imagemCena_3 = loadImage('modulos/layer3.png');
   imagemCena_4 = loadImage('modulos/layer4.png');
-  imagemCena_5 = loadImage('modulos/layer5.png');
-  imagemCena_6 = loadImage('modulos/layer5-2.png');
+
+
 
 }
